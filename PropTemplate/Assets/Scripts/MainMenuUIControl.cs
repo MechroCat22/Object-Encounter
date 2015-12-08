51966e17ea1847cc80480c3bb0e07f9655a8698f
MechroCat22 <chase3@wisc.edu>
2015-12-08 17:41:20 -0600

    Fixed a few things, recovered some work lost

---
+++ PropTemplate/Assets/Scripts/MainMenuUIControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MainMenuUIControl : MonoBehaviour {
+
+    private Camera myCamera;
+	// Use this for initialization
+	void Start () {
+
+        myCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Ray camRay = myCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit objectHit;
+        if (Physics.Raycast(camRay, out objectHit, 60))
+        {
+            GameObject obj = objectHit.transform.gameObject;
+            // aiming at a button
+            if (obj.tag.Equals("Button") && Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("button hit");
+            }
+        }
+    }
+}
